156bfa19ec6456dfb6865337b45664e35f18b866
sonal-uchiru <sonal123326@gmail.com>
2022-05-12 12:23:37 +0530

    cors enabled and bugs fixed

--- src/main/java/com/github/vlsidlyarevich/dto/ChangePasswordDTO.java
+++ src/main/java/com/github/vlsidlyarevich/dto/ChangePasswordDTO.java
@@ -3,8 +3,8 @@ package com.github.vlsidlyarevich.dto;
 public class ChangePasswordDTO {
     private String password;
 
-    public ChangePasswordDTO(String password) {
-        this.password = password;
+    public ChangePasswordDTO() {
+        
     }
 
     public String getPassword() {
